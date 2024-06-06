1e2fe2d19ee7420af42e55733bf37203a72c8bd3
top <js.sookudom@gmail.com>
2024-06-07 00:52:37 +0700

    extract FBox into Data.h

---
+++ game/src/Data.h
@@ -0,0 +1,50 @@
+﻿#pragma once
+
+#include "raylib.h"
+
+#define BOX_SIZE 2.f
+
+typedef struct FBox
+{
+    Vector3 Position;
+    float Scale;
+
+    FBox(float x,float y,float z)
+    {
+        Position = Vector3{x,y,z};
+        Scale = BOX_SIZE;
+    }
+    ~FBox()
+    {
+        
+    }
+    Vector3 GetPosition() const
+    {
+        return Position;
+    }
+    Vector3 GetScale() const
+    {
+        return Vector3{Scale,Scale,Scale};
+    }
+    float GetScalef() const
+    {
+        return Scale;
+    }
+    void SetPosition(const Vector3 NewPosition)
+    {
+        Position = NewPosition;
+    }
+    BoundingBox GetBoundingBox() const
+    {
+        return BoundingBox{
+            Vector3{
+                Position.x - Scale / 2, Position.y - Scale / 2,
+                Position.z - Scale / 2
+            },
+            Vector3{
+                Position.x + Scale / 2, Position.y + Scale / 2,
+                Position.z + Scale / 2
+            }
+        };
+    }
+} FBox;
--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,58 +1,15 @@
+#pragma once
+
 #include "raylib.h"
 #include "camera.h"
+#include "Data.h"
+
 #include <cmath>
 #include <vector>
 
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
-
-#define BOX_SIZE 2.f
-
-typedef struct FBox
-{
-    Vector3 Position;
-    float Scale;
-
-    FBox(float x,float y,float z)
-    {
-        Position = Vector3{x,y,z};
-        Scale = BOX_SIZE;
-    }
-    ~FBox()
-    {
-        
-    }
-    Vector3 GetPosition() const
-    {
-        return Position;
-    }
-    Vector3 GetScale() const
-    {
-        return Vector3{Scale,Scale,Scale};
-    }
-    float GetScalef() const
-    {
-        return Scale;
-    }
-    void SetPosition(const Vector3 NewPosition)
-    {
-        Position = NewPosition;
-    }
-    BoundingBox GetBoundingBox() const
-    {
-        return BoundingBox{
-            Vector3{
-                Position.x - Scale / 2, Position.y - Scale / 2,
-                Position.z - Scale / 2
-            },
-            Vector3{
-                Position.x + Scale / 2, Position.y + Scale / 2,
-                Position.z + Scale / 2
-            }
-        };
-    }
-} FBox;
 
 int main()
 {
